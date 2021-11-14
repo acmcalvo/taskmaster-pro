e9b14eb4b6b7c5a4e9d68c0f5edbfa04029084f2
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2021-11-14 16:39:27 -0500

    change to Interval

--- assets/js/script.js
+++ assets/js/script.js
@@ -245,3 +245,9 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
